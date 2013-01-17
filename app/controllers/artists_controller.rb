20148b9a432af9653e6990235a3a776274fdb846
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2013-01-17 15:28:05 +0800

    Added CRUD for Artist

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,8 +4,43 @@ class ArtistsController < ApplicationController
     @artists = Artist.all
   end
 
+  def new
+    @artist = Artist.new
+  end
+
   def show
     @artist = Artist.find(params[:id])
     @albums = @artist.albums
   end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def create
+    @artist = Artist.new(params[:artist])
+    if @artist.save
+      flash[:success] = "Artist added succesfully"
+      redirect_to artists_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    if @artist.update_attributes(params[:artist])
+      flash[:success] = "Artist updated successfully"
+      redirect_to artists_path
+    end
+  end
+
+  def destroy
+    @artist = Artist.find(params[:id])
+    
+    if @artist.destroy
+      redirect_to artists_path
+    end
+  end
+
 end
