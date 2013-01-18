ebbcafc09c272654eb936a06001e7a5b3c482ab3
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2013-01-18 10:34:02 +0800

    Added CRUD for album

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -6,4 +6,21 @@ class AlbumsController < ApplicationController
   def show
     @album = Album.find(params[:id])
   end
+
+  def new
+    @artist = Artist.find(params[:artist_id])
+    @album = @artist.albums.new
+  end
+
+  def create
+    @artist = Artist.find(params[:artist_id])
+    @album = Album.new(params[:album])
+    if @album.save
+      flash[:success] = "Album successfully created!"
+      redirect_to artist_album_path(@artist, @album)
+    else
+      render "new"
+    end
+
+  end
 end
